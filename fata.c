9760f362ea465fecf7c98d25fe9fde69d8bcb6ca
Crius Gamora <cgamora@oa-c3.21-school.ru>
2020-02-06 22:06:25 +0300

    g

---
+++ fata.c
@@ -0,0 +1,31 @@
+void     ft_date(int i)
+{
+    int day;
+    int year;
+    int month;
+    int zl;
+    int hour;
+    int min;
+    char stre[19];
+
+    ft_putstr(ft_itoa(year = i/31436000+1970)); 
+    zl = (year-1969)/4;
+    month = i/2629743 + 1;
+    ft_putchar('-');
+    ft_putstr(ft_itoa(month = month%12));
+    ft_putchar('-');    
+    day = i/86400;
+    i = i - day*86400;
+    day -= zl;
+    day %= 365;
+    ft_putstr(ft_itoa(day = ft_month(day, year%4>0 ? 0:1)));
+    ft_putchar('T');
+    ft_putstr(ft_itoa(hour = i/3600));
+    ft_putchar(':');  
+    i -= hour*3600;
+    ft_putstr(ft_itoa(min = i/60));
+    i -= min*60;
+    ft_putchar(':');
+    ft_putstr(ft_itoa(i));
+    ft_putstr("\n");
+}
--- test.c
+++ test.c
@@ -1,64 +1,126 @@
 #include "ft_printf.h"
 
-int     ft_month(int day, int flag)
+int     ft_month(int ost, int flag)
 {
-    if (day < 31)
-        return (day);
-    if (day < 59 + flag)
-        return (day - 31);
-    if (day < 90 + flag)
-        return (day- 59);
-    if (day < 120+ flag)
-        return (day - 90);
-    if (day < 151+ flag)
-        return (day - 120);
-    if (day < 181+ flag)
-        return (day - 151);
-    if (day < 212+ flag)
-        return (day - 181);
-    if (day < 243+ flag)
-        return (day - 212);
-    if (day < 273+ flag)
-        return (day - 243);
-    if (day < 304+ flag)
-        return (day - 273);
-    if (day < 334+ flag)
-        return (day - 304);
-    if (day <= 365+ flag)
-        return (day - 334);
+    if (ost <= 2678400)
+        return (1);
+    if (ost <= 5097600 + (flag*86400))
+        return (2);
+    if (ost <= 7776000 + (flag*86400))
+        return (3);
+    if (ost <= 10368000+(flag*86400))
+        return (4);
+    if (ost <= 13046400+(flag*86400))
+        return (5);
+    if (ost <= 15638400+ (flag*86400))
+        return (6);
+    if (ost <= 18316800 +(flag*86400))
+        return (7);
+    if (ost <= 20995200 +(flag*86400))
+        return (8);
+    if (ost <= 23587200 + (flag*86400))
+        return (9);
+    if (ost <= 26265600 +(flag*86400))
+        return (10);
+    if (ost <= 28857600 +(flag*86400))
+        return (11);
+    if (ost <= 31536000 +(flag*86400))
+        return (12);
     return (0);
 }
 
-void     date(int i)
+int     ft_day(int day, int flag)
 {
+    if (day <= 31)
+        return (day);
+    if (day <= 59 + flag)
+        return (day - 31);
+    if (day <= 90 + flag)
+        return (day- (59 + flag));
+    if (day <= 120+ flag)
+        return (day - (90 + flag));
+    if (day <= 151+ flag)
+        return (day - (120 + flag));
+    if (day <= 181+ flag)
+        return (day - (151 + flag));
+    if (day <= 212+ flag)
+        return (day - (181 + flag));
+    if (day <= 243+ flag)
+        return (day - (212 + flag));
+    if (day <= 273+ flag)
+        return (day - (243 + flag));
+    if (day <= 304+ flag)
+        return (day - (273 + flag));
+    if (day <= 334+ flag)
+        return (day - (304 + flag));
+    if (day <= 365+ flag)
+        return (day - (334 + flag));
+    return (0);
+}
+
+void ft_date(int n)
+{
+    int i;
     int day;
-    int year;
     int month;
-    int zl;
     int hour;
     int min;
-    char stre[19];
+    int k = 0,l = 0;
 
-    ft_putstr(ft_itoa(year = i/31436000+1970)); 
-    zl = (year-1969)/4;
-    month = i/2629743 + 1;
-    ft_putchar('-');
-    ft_putstr(ft_itoa(month = month%12));
-    ft_putchar('-');    
-    day = i/86400;
-    i = i - day*86400;
-    day -= zl;
-    day %= 365;
-    ft_putstr(ft_itoa(day = ft_month(day, year%4>0 ? 0:1)));
-    ft_putchar('T');
-    ft_putstr(ft_itoa(hour = i/3600));
-    ft_putchar(':');  
-    i -= hour*3600;
-    ft_putstr(ft_itoa(min = i/60));
-    i -= min*60;
-    ft_putchar(':');
-    ft_putstr(ft_itoa(i));
-    ft_putstr("\n");
+    i = 0;
+    day = 0;
+    while (n > 0)
+    {
+        if (n > 0)
+        {
+            n -= 31536000;
+            i++;
+            k++;
+        }
+        if (n > 0)
+        {
+            n -= 31536000;
+            i++;
+        }
+        if (n > 0)
+        {
+            n -= 31536000;
+            i++;
+        }
+        if (n > 0)
+        {
+            n -= 31622400;
+            i++;
+            l++;
+        }
+    }
+    
+    if(k==l)
+        n += 31622400;
+    else
+        n += 31536000;
+    i -= 2;
+    month = 0;
+    month = ft_month(n,(1970+i)%4==0?1:0);
+    while(n > 0)
+    {
+        day++;
+        n -= 86400;
+    }
+        n += 86400;
+    day = ft_day(day,(1970+i)%4==0?1:0);
+    hour = 0;
+    hour = n/3600;
+    n  -= hour * 3600;
+    min = 0;
+    min = n/60;
+    n -= min * 60;
+    printf("\nGOD: %d OST: %d",i + 1970,n);
+    printf("\nMONTH: %d",month);
+    printf("\nDAY: %d\n", day);
+    printf("HOUR: %d",hour);
+    printf("\nMINUTS: %d",min);
+    printf("\nSEC: %d",n);
 }
 
 int     ft_printf(const char* format,...)
@@ -95,19 +157,7 @@ int     main()
     char c = '3';
     // ft_printf("%d%d%d%d",40,0,42,49,44);
     // printf("%'i",32423423);
-    date(1584652923);
-    date(1584566523);
-    date(1584566523);
-    date(1552944123);
-    date(1521408123);
-    date(1489872123);
-    date(1458336123);
-    date(1426713723);
-    date(1395177723);
-    date(1363641723);
-    date(1332105723);
-    date(1580922278 );
-    date(1549382710);
+    ft_date(704712733);
     //printf("\n%d%d%d%2$d\n",34,42,21);
     // printf("%5d\n",44);
     // printf("%.2d\n",44);
